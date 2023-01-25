ef8624f3713090f7cdc432cdb62cf30d33e97ba1
Michael Vargas <Michael.Vargas0290@gmail.com>
2023-01-25 14:56:43 -0600

    Create Header component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,79 @@
+import {
+    Button,
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    Row,
+    Col,
+    Container,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelopesBulk } from "@fortawesome/free-solid-svg-icons";
+import styles from "./Header.module.css";
+
+const Header = () => {
+    return (
+        <Container className={styles.nav_section} fluid>
+            <Row className={styles.logo_section}>
+                <Col md="2" className="py-3">
+                    <FontAwesomeIcon icon={faEnvelopesBulk} size="3x" />
+                </Col>
+                <Col className="py-3">
+                    <h1 className="text-start">MailTech</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="px-0">
+                    <Navbar className={styles.navbar}>
+                        <Nav>
+                            <NavItem>
+                                <NavLink>
+                                    <Button color="primary">
+                                        Report Production
+                                    </Button>
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink>
+                                    <Button color="primary">
+                                        My Production Report
+                                    </Button>
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink>
+                                    <Button color="primary">
+                                        Production Reports
+                                    </Button>
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink>
+                                    <Button color="primary">
+                                        Update Hours Worked
+                                    </Button>
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink>
+                                    <Button color="primary">
+                                        Volume Reports
+                                    </Button>
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink>
+                                    <Button color="primary">Log Out</Button>
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Navbar>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Header;

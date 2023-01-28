aa536a06b73923e7ec7691b7709c5338110f5143
Michael Vargas <Michael.Vargas0290@gmail.com>
2023-01-27 20:36:43 -0600

    Create LoginForm Component

---
+++ src/Components/Forms/LoginForm.js
@@ -0,0 +1,48 @@
+import { Button, Label, Col, FormGroup } from "reactstrap";
+import { Formik, Field, Form } from "formik";
+
+const LoginForm = () => {
+    const handleSubmit = (values, { resetForm }) => {
+        console.log("form values:", values);
+        console.log("in JSON format;", JSON.stringify(values));
+        resetForm();
+    };
+
+    return (
+        <Formik
+            initialValues={{
+                userName: "",
+                password: "",
+            }}
+            onSubmit={handleSubmit}
+        >
+            <Form>
+                <FormGroup row>
+                    <Label htmlFor="userName">Username</Label>
+                    <Col md="12">
+                        <Field
+                            name="userName"
+                            placeholder="Username"
+                            className="form-control"
+                        />
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label htmlFor="password">Password</Label>
+                    <Col md="12">
+                        <Field
+                            name="password"
+                            placeholder="Password"
+                            className="form-control"
+                        />
+                    </Col>
+                </FormGroup>
+                <Button type="submit" color="primary">
+                    Login
+                </Button>
+            </Form>
+        </Formik>
+    );
+};
+
+export default LoginForm;

83735fa38b4ecc365a333a8670811f8337b44841
Michael Vargas <Michael.Vargas0290@gmail.com>
2023-01-28 13:55:02 -0600

    Add Form component to ReportProductionPage.js. Create and add styles.

--- src/Pages/ReportProductionPage/ReportProductionPage.js
+++ src/Pages/ReportProductionPage/ReportProductionPage.js
@@ -1,5 +1,22 @@
+import { Col, Container, Row } from "reactstrap";
+import ReportProductionForm from "../../Components/Forms/ReportProductionForm";
+import styles from "../ReportProductionPage/ReportProductionPage.module.css";
+
 const ReportProductionPage = () => {
-    return <h1>Report Production</h1>;
+    return (
+        <Container>
+            <Row>
+                <Col md="12">
+                    <h1 className={styles.report_production_page_header}>
+                        Report Production
+                    </h1>
+                </Col>
+                <Col md="12">
+                    <ReportProductionForm />
+                </Col>
+            </Row>
+        </Container>
+    );
 };
 
 export default ReportProductionPage;

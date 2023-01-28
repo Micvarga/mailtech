d57ad85377ac2771ad962e80075646732719221c
Michael Vargas <Michael.Vargas0290@gmail.com>
2023-01-27 20:38:06 -0600

    Fixed error with React Router set up.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
-import LoginPage from "./Pages/LoginPage";
+import LoginPage from "./Pages/LoginPage/LoginPage";
 import MyEfficiencyReportsPage from "./Pages/MyEfficiencyReports";
 import TeamMetricsReportsPage from "./Pages/TeamMetricsReportsPage";
 import ReportProductionPage from "./Pages/ReportProductionPage";
@@ -15,23 +15,13 @@ function App() {
             <Header />
             <Routes>
                 <Route path="/" element={<LoginPage />} />
-            </Routes>
-            <Routes>
                 <Route
-                    path="efficiency"
+                    path="/efficiency"
                     element={<MyEfficiencyReportsPage />}
                 />
-            </Routes>
-            <Routes>
                 <Route path="production" element={<ReportProductionPage />} />
-            </Routes>
-            <Routes>
                 <Route path="metrics" element={<TeamMetricsReportsPage />} />
-            </Routes>
-            <Routes>
                 <Route path="volume" element={<VolumeReportsPage />} />
-            </Routes>
-            <Routes>
                 <Route path="hours" element={<WorkedHoursReportsPage />} />
             </Routes>
         </div>

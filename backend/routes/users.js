f84db136fb03e75caa69666552b2add934a7a7a2
Michael Vargas <Michael.Vargas0290@gmail.com>
2023-02-01 15:50:11 -0600

    Fixed error in login route.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -39,7 +39,7 @@ router.post("/login", passport.authenticate("local"), (req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
     res.json({
-        sucess: true,
+        success: true,
         token: token,
         status: "You are successfully logged in!",
     });

9acffd14e96884a2b1903375069510646662e813
Michael Vargas <Michael.Vargas0290@gmail.com>
2023-02-01 13:15:33 -0600

    Add JWT authentication to Users Route.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const User = require("../models/user");
 const passport = require("passport");
+const authenticate = require("../authenticate");
+
 const router = express.Router();
 
 /* GET users listing. */
@@ -32,10 +34,15 @@ router.post("/signup", (req, res) => {
     );
 });
 
-router.post("/login", (req, res) => {
+router.post("/login", passport.authenticate("local"), (req, res) => {
+    const token = authenticate.getToken({ _id: req.user._id });
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
-    res.json({ sucess: true, status: "You are successfully logged in!" });
+    res.json({
+        sucess: true,
+        token: token,
+        status: "You are successfully logged in!",
+    });
 });
 
 router.get("/logout", (req, res, next) => {

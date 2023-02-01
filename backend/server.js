55ef0663582b7b0a0bca62660966c4cb2ac89420
Michael Vargas <Michael.Vargas0290@gmail.com>
2023-01-31 21:43:14 -0600

    Implement passport-local strategy in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const passport = require("passport");
+const authenticate = require("./authenticate");
 const secret = require("./secret");
 
 const indexRouter = require("./routes/index");
@@ -33,11 +37,38 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(cookieParser());
+app.use(
+    session({
+        name: "session-id",
+        secret: "1234-67890-09876-54321",
+        saveUninitialized: false,
+        resave: false,
+        store: new FileStore(),
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+function auth(req, res, next) {
+    console.log(req.user);
+
+    if (!req.user) {
+        const err = new Error("You are not authenticated!");
+        err.status = 401;
+        return next(err);
+    } else {
+        return next();
+    }
+}
+
+app.use(auth);
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {

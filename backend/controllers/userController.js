f1dd90f11068dbb6f95e8e530ee2378799b93d66
Michael Vargas <Michael.Vargas0290@gmail.com>
2023-02-21 16:34:04 -0600

    Create userController.js

---
+++ backend/controllers/userController.js
@@ -0,0 +1,75 @@
+const user = require("../models/user");
+const passport = require("passport");
+const authenticate = require("../authenticate");
+
+const getUsers = (req, res, next) => {
+    user.find()
+        .then((users) => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json(users);
+        })
+        .catch((err) => next(err));
+};
+
+const signUpUser = (req, res, next) => {
+    console.log(req.body.username);
+    console.log(req.body.firstName);
+    console.log(req.body.lastName);
+    user.register(
+        new user({
+            username: req.body.username,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+        }),
+        req.body.password,
+        (err) => {
+            if (err) {
+                res.statusCode = 500;
+                res.setHeader("Content-Type", "application/json");
+                res.json({ err: err });
+            } else {
+                passport.authenticate("local")(req, res, () => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json({
+                        success: true,
+                        status: "Registration Successful!",
+                    });
+                });
+            }
+        }
+    );
+};
+
+const loginUser =
+    (passport.authenticate("local"),
+    (req, res, next) => {
+        const token = authenticate.getToken({ _id: req.user._id });
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json({
+            success: true,
+            token: token,
+            status: "You are successfully logged in!",
+        });
+    });
+
+const logoutUser = (req, res, next) => {
+    if (req.session) {
+        req.session.destroy();
+        res.clearCookie("session-id");
+        res.redirect("/");
+    } else {
+        const err = new Error("You are not logged in!");
+        err.status = 401;
+        return next(err);
+    }
+};
+
+module.exports = {
+    getUsers,
+    signUpUser,
+    loginUser,
+    logoutUser,
+};

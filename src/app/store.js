a30222f41c0a94bcbed8f038ffefbe776ff17e4f
Michael Vargas <Michael.Vargas0290@gmail.com>
2023-03-24 11:18:56 -0500

    Add RTKQ getUserDetailsQuery reducer and middleware to Redux Store.

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import userReducer from "../features/Users/userSlice";
+import { authApi } from "./Services/authServices";
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
+        [authApi.reducerPath]: authApi.reducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(authApi.middleware),
 });

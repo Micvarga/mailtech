c45ef0b915e33cc6fe430f8fcf3c8a162885fb14
Michael Vargas <Michael.Vargas0290@gmail.com>
2023-03-24 11:15:53 -0500

    Update userSlice with login reducers.

--- src/features/Users/userSlice.js
+++ src/features/Users/userSlice.js
@@ -29,22 +29,66 @@ export const registerUser = createAsyncThunk(
     }
 );
 
+export const userLogin = createAsyncThunk(
+    "user/login",
+    async ({ username, password }, { rejectWithValue }) => {
+        try {
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            };
+            const { data } = await axios.post(
+                `${backendUrl}/users/login`,
+                { username, password },
+                config
+            );
+
+            localStorage.setItem("userToken", data.userToken);
+            return data;
+        } catch (error) {
+            if (error.response && error.response.data.message) {
+                return rejectWithValue(error.response.data.message);
+            } else {
+                return rejectWithValue(error.message);
+            }
+        }
+    }
+);
+
+// initialize userToken from local storage
+const userToken = localStorage.getItem("userToken")
+    ? localStorage.getItem("userToken")
+    : null;
+
 const initialState = {
     loading: false,
-    userInfo: {},
-    userToken: null,
-    errorMsg: null,
-    success: false,
+    userInfo: null, //for storing user object.
+    userToken: null, //for storing the JWT.
+    error: null,
+    success: false, //for monitoring backend call process.
 };
 
 const userSlice = createSlice({
     name: "user",
     initialState,
-    reducers: {},
+    reducers: {
+        logout: (state) => {
+            localStorage.removeItem("userToken"); //deletes token from storage
+            state.loading = false;
+            state.userInfo = null;
+            state.userToken = null;
+            state.error = null;
+        },
+        setCredentials: (state, { payload }) => {
+            state.userInfo = payload;
+        },
+    },
     extraReducers: {
+        // register user lifecycle action types
         [registerUser.pending]: (state) => {
             state.loading = true;
-            state.errorMsg = null;
+            state.error = null;
         },
         [registerUser.fulfilled]: (state, { payload }) => {
             state.loading = false;
@@ -52,9 +96,25 @@ const userSlice = createSlice({
         },
         [registerUser.rejected]: (state, { payload }) => {
             state.loading = false;
-            state.errorMsg = payload;
+            state.error = payload;
+        },
+        // login user lifecycle action types
+        [userLogin.pending]: (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+        [userLogin.fulfilled]: (state, { payload }) => {
+            state.loading = false;
+            state.userInfo = payload;
+            state.userToken = payload.userToken;
+        },
+        [userLogin.rejected]: (state, { payload }) => {
+            state.loading = false;
+            state.error = payload;
         },
     },
 });
+
+export const { logout, setCredentials } = userSlice.actions;
 
 export default userSlice.reducer;
